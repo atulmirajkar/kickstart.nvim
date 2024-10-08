d409206e8b32861d29018cce0ebeb0bc16da012e
amirajka <atul@epic.com>
2024-10-08 01:01:10 -0500

    fix typo in cmp

--- lua/kickstart/cmp.lua
+++ lua/kickstart/cmp.lua
@@ -44,36 +44,36 @@ M.config = function()
   require('luasnip.loaders.from_vscode').lazy_load()
   luasnip.config.setup {}
 
-  ---@diagnostic disable-next-line: missing-fields
-  cmp.setup {
-    preselect = cmp.PreselectMode.None,
-    completion = {
-      completeopt = 'menu,menuone,noinsert,preview',
-    },
-    snippet = {
-      expand = function(args)
-        luasnip.lsp_expand(args.body)
-      end,
-    },
     ---@diagnostic disable-next-line: missing-fields
-    -- completion = { completeopt = 'menu,menuone,noinsert' },
-    mapping = cmp.mapping.preset.insert {
-      ['<Down>'] = cmp.mapping.select_next_item(),
-      ['<Up>'] = cmp.mapping.select_prev_item(),
-      ['<C-n>'] = cmp.mapping.select_next_item(),
-      ['<C-p>'] = cmp.mapping.select_prev_item(),
-      ['<C-f>'] = cmp.mapping.scroll_docs(-4),
-      ['<C-d>'] = cmp.mapping.scroll_docs(4),
-      ['<C-Space>'] = cmp.mapping.complete {},
-      ['<CR>'] = cmp.mapping.confirm {
-        behavior = cmp.ConfirmBehavior.Replace,
-        select = true,
-      },
-      ['<C-y>'] = cmp.mapping.confirm {
-        behavior = cmp.ConfirmBehavior.Replace,
-        select = true,
-      },
-      ['<C-e'] = cmp.mapping.abort(),
+    cmp.setup {
+        preselect = cmp.PreselectMode.None,
+        completion = {
+            completeopt = 'menu,menuone,noinsert,preview',
+        },
+        snippet = {
+            expand = function(args)
+                luasnip.lsp_expand(args.body)
+            end,
+        },
+        ---@diagnostic disable-next-line: missing-fields
+        -- completion = { completeopt = 'menu,menuone,noinsert' },
+        mapping = cmp.mapping.preset.insert {
+            ['<Down>'] = cmp.mapping.select_next_item(),
+            ['<Up>'] = cmp.mapping.select_prev_item(),
+            ['<C-n>'] = cmp.mapping.select_next_item(),
+            ['<C-p>'] = cmp.mapping.select_prev_item(),
+            ['<C-f>'] = cmp.mapping.scroll_docs(-4),
+            ['<C-d>'] = cmp.mapping.scroll_docs(4),
+            ['<C-Space>'] = cmp.mapping.complete {},
+            ['<CR>'] = cmp.mapping.confirm {
+                behavior = cmp.ConfirmBehavior.Replace,
+                select = true,
+            },
+            ['<C-y>'] = cmp.mapping.confirm {
+                behavior = cmp.ConfirmBehavior.Replace,
+                select = true,
+            },
+            ['<C-e>'] = cmp.mapping.abort(),
 
       -- Think of <c-l> as moving to the right of your snippet expansion.
       --  So if you have a snippet that's like:
