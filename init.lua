7f7bd79b6c2ae704cbba243b06c1d0bd1941dea5
atulmirajkar <atulmirajkar@gmail.com>
2024-11-17 10:12:32 -0600

    add lazygit

--- init.lua
+++ init.lua
@@ -20,6 +20,8 @@ Spec 'custom.plugins.copilot-lualine'
 Spec 'custom.plugins.copilot-chat'
 Spec 'custom.plugins.tmux-navigator'
 Spec 'custom.plugins.project'
+Spec 'custom.plugins.lazygit'
+
 --kickstart
 Spec 'kickstart.devicons'
 Spec 'kickstart.colorscheme'
