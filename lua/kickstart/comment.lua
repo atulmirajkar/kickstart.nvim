4a1223d18e29a59efb4d0be42710eaa6b077e607
atulmirajkar <atulmirajkar@gmail.com>
2024-11-22 23:14:40 -0600

    colorscheme to kanagawa

--- lua/kickstart/comment.lua
+++ lua/kickstart/comment.lua
@@ -1,15 +1,14 @@
 local M = {
-    'numToStr/Comment.nvim',
-    dependencies = {
-        'folke/todo-comments.nvim',
-        'nvim-lua/plenary.nvim'
-    },
-    lazy = false,
+  'numToStr/Comment.nvim',
+  dependencies = {
+    'folke/todo-comments.nvim',
+    'nvim-lua/plenary.nvim',
+  },
+  lazy = false,
 }
 M.config = function()
-    require('Comment').setup{}
-    require('todo-comments').setup {
-        signs = false
-    }
+  require('todo-comments').setup {
+    signs = false,
+  }
 end
 return M
